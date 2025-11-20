5110e4086ed88a11a9b18317881319a47fc0ebe6
bijay <bijaysunar506@gmail.com>
2025-11-20 13:13:06 +0545

    Even or odd

--- Day_1/script.js
+++ Day_1/script.js
@@ -8,3 +8,24 @@ console.log(program)
 //Constant in Js
 const pi=3.14;
 console.log(pi);
+
+
+//function in js
+
+function add(a,b) {
+    return a + b;
+
+}
+console.log(add( 5,3));
+
+
+//
+
+
+const evenodd = (num)=>{
+    if(num %2 == 0) {
+        return console.log(`${num}is Even`)
+    }else{
+        return console.log(`${num}is Odd`);
+    }
+}
